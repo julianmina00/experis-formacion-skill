bb25aecef5ffaeb04d0d0dcd038c7808b64c0b4b
Julian Mina Caicedo <julianandres.mina@experis-spain.com>
2020-02-08 18:31:38 +0100

    adding the alexa contents to the git repo

---
+++ alexa/lambda/localisation.js
@@ -0,0 +1,24 @@
+module.exports = {
+    es: {
+        translation: {
+          WELCOME_MSG:  'Bienvenido a Experis Formación. Para empezar, debes registrarte como usuario',
+          USUARIO_REGISTRADO_MSG: 'Perfecto! Ya estás registrado en Experis Formación',
+          HELP_MSG: '',
+          GOODBYE_MSG: 'Hasta luego!',
+          REFLECTOR_MSG: 'Acabas de activar %s',
+          FALLBACK_MSG: 'Lo siento, no se nada sobre eso. Por favor inténtalo otra vez.',
+          ERROR_MSG: 'Lo siento, ha habido un problema. Por favor inténtalo otra vez.'
+        }
+    },
+    en: {
+        translation: {
+          WELCOME_MSG:  'Bienvenido a Experis Formación. Para empezar, debes registrarte como usuario',
+          USUARIO_REGISTRADO_MSG: 'Perfecto! Ya estás registrado en Experis Formación',
+          HELP_MSG: '',
+          GOODBYE_MSG: 'Hasta luego!',
+          REFLECTOR_MSG: 'Acabas de activar %s',
+          FALLBACK_MSG: 'Lo siento, no se nada sobre eso. Por favor inténtalo otra vez.',
+          ERROR_MSG: 'Lo siento, ha habido un problema. Por favor inténtalo otra vez.'
+        }
+    }
+}
